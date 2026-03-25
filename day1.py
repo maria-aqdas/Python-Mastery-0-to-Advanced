9265d8b5d337fce259529a94f699517c54fc3056
maria-aqdas <mariaaqdas786@gmail.com>
2026-03-25 16:01:35 +0500

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,27 @@
+print("<<ALLAH ALMIGHTY>>")
+
+#INTEGER DATA TYPE
+a=12
+print(a)
+#STRING DATA TYPE
+b="Allah"
+print(b)
+#FLOAT DATA TYPE
+c=1.2
+print(c)
+#bOOLEAN DATA TYPE
+d=True
+print(d)
+
+e="My name is Maria Aqdas. I love Allah so much. Allah is Heart. Allah is my All money. "
+print("Maria is says:  ", e)
+
+#PRINT FUNCTION
+print("ALLAH"," Muhammad",sep="-")
+print("ALLAH"," Muhammad",sep="/")
+print("ALLAH"," Muhammad",sep="--")
+print("ALLAH"," Muhammad",sep="__")
+
+print("Allah ",end=" ")
+print("Almighty.")
+
