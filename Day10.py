1eb7da6f707a252f9634f3ee2009e01163fb0175
maria-aqdas <mariaaqdas786@gmail.com>
2026-04-05 15:13:11 +0500

    Add files via upload

---
+++ Day10.py
@@ -0,0 +1,45 @@
+# my_favorites = ["Cricket", "Biryani", "Python", "Gaming", "Lahore"]
+# print(my_favorites[2])
+# print(my_favorites[-1])
+
+# week = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+# print(week[0:3])
+# print(week[5:])
+
+# stationary=["raser", "remover", "ball point" ,"pencil","shapran","pen","color"]
+# stationary.append("marker")
+# print(stationary)
+# stationary.remove("pencil")
+# print(stationary)
+# stationary.sort()
+# print(stationary)
+
+
+# my_marks=[78,85,92,88,95]
+# total_marks=sum(my_marks)
+# total_length=len(my_marks)
+# average_marks=total_marks/total_length
+# print(f"The marks of 5 subjects is: {average_marks}.")
+
+# board = [
+#     ["X", "O", "X"], # Row 0
+#     ["O", "X", "O"], # Row 1
+#     ["O", "O", "X"]  # Row 2
+# ]
+# print(board[1][1])
+
+shopping_list=[]
+shopping_list.append("apple")
+shopping_list.append("milk")
+shopping_list.append("bread")
+shopping_list.append("eggs")
+print(shopping_list)
+shopping_list.remove("bread")
+print(shopping_list)
+shopping_list.pop()
+print(shopping_list)
+shopping_list.sort()
+print(shopping_list)
+length=len(shopping_list)
+print(length)
+print(f"The total shoping items are {length} and their name are {shopping_list}")
